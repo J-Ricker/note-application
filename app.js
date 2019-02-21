0509525e59546a09d3555a83fd22a24394552e73
J-Ricker <lvlgrinddesign@gmail.com>
2019-02-21 13:17:58 -0500

    cleaned up files for application build

--- app.js
+++ app.js
@@ -2,24 +2,7 @@
 console.log('starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
 const notes = require('./notes')
 
-// console.log(_.isString(true));
-// console.log(_.isString('Andrew'));
-var filteredArray = _.uniq(['jeremy', 1, 'jeremy', 1, 2, 3, 4]);
-console.log(filteredArray);
-
-
-// var result = notes.add(2, 5);
-// console.log(`Result: ${result}`);
-
-
-// var user = os.userInfo();
-
-// fs.appendFile('greetings.txt', `hello ${user.username}! you are ${notes.age}.`, function (err) {
-//     if (err) {
-//         console.log('unable to write to file');
-//     }
-// });
