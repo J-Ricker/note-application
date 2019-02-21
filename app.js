1511ec6458811ab198a4c02bd0b8301c02912536
J-Ricker <lvlgrinddesign@gmail.com>
2019-02-21 12:40:08 -0500

    testing require for note.js to log the console when starting app.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 // intialization file
-console.log('starting app');
+console.log('starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes')
 
 var user = os.userInfo();
-console.log(user);
+
 fs.appendFile('greetings.txt', `hello ${user.username}!`, function (err) {
     if (err) {
         console.log('unable to write to file');
