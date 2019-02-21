21d0d39f103cb243a5c6c97e1f9fa62007309186
J-Ricker <lvlgrinddesign@gmail.com>
2019-02-21 15:01:52 -0500

    Updated add note to create new json file with note title and body stored

--- notes.js
+++ notes.js
@@ -1,6 +1,33 @@
 console.log('starting notes.js');
+const fs = require('fs');
 
-module.exports.addNote = () => {
-    console.log('addNote');
-    return 'new note';
+
+var addNotes = (title, body) => {
+    var notes = [];
+    var note = {
+        title,
+        body
+    };
+
+    notes.push(note);
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
+var getAll = () => {
+    console.log('Getting all notes');
+};
+
+var getNote = (title) => {
+    console.log(`Getting note: ${title}`);
+}
+
+var removeNote = (title) => {
+    console.log(`Are you sure you want to remove note: ${title}?`);
+}
+
+module.exports = {
+    addNotes,
+    getAll,
+    getNote,
+    removeNote
 };
