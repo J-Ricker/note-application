a18645b1cab72c6e2769c8ef569cf3c3f726149a
J-Ricker <lvlgrinddesign@gmail.com>
2019-02-21 13:31:45 -0500

    practicing using process.argv

--- app.js
+++ app.js
@@ -6,3 +6,18 @@ const _ = require('lodash');
 
 const notes = require('./notes')
 
+var command = process.argv[2];
+console.log('Command: ', command);
+console.log(process.argv);
+
+if (command === 'add') {
+    console.log('adding new note');
+} else if (command === 'list') {
+    console.log('Listing all notes');
+} else if (command === 'read') {
+    console.log('Reading note');
+} else if (command === 'remove') {
+    console.log('removing note');
+} else {
+    console.log('Command not recongnized');
+}
